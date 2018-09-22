6a77be59dc698c48d0a32a9a2c709dd4967010be
wahidherlambang <43497169+wahidherlambang@users.noreply.github.com>
2018-09-22 23:25:27 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117064/latihan10/logika/PBO210117064Latihan10Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117064.latihan10.logika;
+
+/**
+ *
+ * @author WAHID HERLAMBANG
+ * NAMA     : Wahid Herlambang Suroso
+ * KELAS    : PBO2
+ * NIM      : 10117064
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * operator logika
+ * 
+ */
+public class PBO210117064Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
